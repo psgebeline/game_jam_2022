0cfa3190ba881f462dae2bd2ee88720de9346e8e
Paul Gebeline <paulgebeline1@gmail.com>
2022-07-09 22:27:57 -0500

    tried to implement life counter.. not going so well...

--- game_2d/Assets/Scripts/Game 1/game1_death_script.cs
+++ game_2d/Assets/Scripts/Game 1/game1_death_script.cs
@@ -7,15 +7,31 @@ public class game1_death_script : MonoBehaviour
 
     public GameObject startPoint;
     public GameObject Player; //storing game objects
+    public GameObject[] life; 
+    private int i = 0;
 
 
    
    
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if(other.gameObject.CompareTag("Player")) 
         {
-            Player.transform.position = startPoint.transform.position; //when this object has a collision, if the other object is the player, respawn them at the empty gameobject spawn point
+            Player.transform.position = startPoint.transform.position;
+            //Debug.Log(life[i]);
+            //if(i <=2)
+            {
+                //Player.transform.position = startPoint.transform.position; //when this object has a collision, if the other object is the player, respawn them at the empty gameobject spawn point
+                //Destroy(life[i]);
+                //i++;
+                
+            }
+            //else
+            {
+                //end game
+            }
+            
+            
         }
-    }
+    }   
 }
