ea9968b53041c7e45fd8a436239fa22851272e61
Paul Gebeline <paulgebeline1@gmail.com>
2022-07-11 18:55:57 -0500

    bugfixes, called speed/accel from other script and reset them upon collisions to prevent constantly speeding up

--- game_2d/Assets/Obstacle_Collisions.cs
+++ game_2d/Assets/Obstacle_Collisions.cs
@@ -6,23 +6,36 @@ public class Obstacle_Collisions : MonoBehaviour
 {
 
 
-    public GameObject startPoint;
+    public GameObject startPoint; //let us set the start point, the player, and the array of lives which are referenced later. also set up i to iterate.
     public GameObject player;
     public GameObject[] life; 
     private int i = 0;
-
+    private game1_movement movement_script; //allows us to reference speed/acceleration from the movement script
 
     // Start is called before the first frame update
   
     // Update is called once per frame
     
-    private void OnCollisionEnter2D(Collision2D other)
+    private void Start()
+    {
+        movement_script = player.GetComponent<game1_movement>();
+        // on first frame, stores game1_movement script
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other) //when the player collides with something..
     {
         if(other.gameObject.CompareTag("Obstacle")) 
         {
-            player.transform.position = startPoint.transform.position; //when this object has a collision, if the other object is the player, respawn them at the empty gameobject spawn point
-            life[i].SetActive(false); //set the ith life indicator to be invisible and then iterate i
-            i++;
+            player.transform.position = startPoint.transform.position; //when player has a collision, if the other object is an obstacle, respawn them at the empty gameobject spawn point
+
+            life[i].SetActive(false); //set the ith life indicator to be invisible, starting at i=0
+
+            i++; //iterate i so that the next collision will turn off the 2nd life, etc
+
+            movement_script.speed = 3; //when character respawns set their speed to the starting speed (3) and acceleration to 0
+            movement_script.acceleration = 0;
+
             if(i > 2);
             {
                 //end game
