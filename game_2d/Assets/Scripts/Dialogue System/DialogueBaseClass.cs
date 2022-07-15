69573be3a2be5928166a8c3f1e74aecb9288fb94
Paul Gebeline <paulgebeline1@gmail.com>
2022-07-15 18:04:26 -0500

    created a script to manage dialogue presentation

---
+++ game_2d/Assets/Scripts/Dialogue System/DialogueBaseClass.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueBaseClass : MonoBehaviour
+    {
+        //this coroutine is responsible for showing letters one by one. essentially
+        //it loops over every letter, putting the letter into the textholder and waiting
+        //0.1 seconds before displaying the next letter.
+        
+        private IEnumerator WriteText(string input, Text textHolder)
+        {
+            for(int i = 0; i < input.Length; i++)
+            {
+                textHolder.text += input[i];
+                yield return new WaitForSeconds(0.1f);
+            }
+
+        }
+    }
+
+
+}
+
+
